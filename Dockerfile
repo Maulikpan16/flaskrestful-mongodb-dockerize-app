60bdde619ab9305e87fcf120fc5aa238a513616f
Maulikpan16 <panchalmaulik1999@gmail.com>
2020-12-28 18:53:46 +0530

    ok

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,5 @@
-FROM python:3.6
+FROM python:3.8
 COPY . /app
 WORKDIR /app
 RUN pip install -r requirements.txt
-EXPOSE 5000
-ENTRYPOINT [ "python" ]
-CMD [ "main.py" ]
+CMD ["uwsgi", "app.ini"]
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask_restful import Api, Resource
 from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
+
 # hello is my collection name
 app = Flask(__name__)
 api = Api(app)
+
 # define path for our mongodb database
 app.config["MONGO_URI"] = "mongodb://mongo:27017/maulik"
 #initialize pymongo with mongo object
